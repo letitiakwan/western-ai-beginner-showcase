c567abe3cba58c1d3a72ad57266fc415c87708a7
Letitia <letitia.kwan@gmail.com>
2020-04-11 10:31:58 -0400

    Updates

---
+++ precache-manifest.9dc0b37e496156d45d465ad26d686ff1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a75891c8afb7e4efaba09fdaaa4936",
+    "url": "/western-ai-beginner-showcase/index.html"
+  },
+  {
+    "revision": "7fc213b7b63827512304",
+    "url": "/western-ai-beginner-showcase/static/css/2.e61fc129.chunk.css"
+  },
+  {
+    "revision": "9d8350c793be79cae552",
+    "url": "/western-ai-beginner-showcase/static/css/main.8c0dfe4e.chunk.css"
+  },
+  {
+    "revision": "7fc213b7b63827512304",
+    "url": "/western-ai-beginner-showcase/static/js/2.50b8c935.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/western-ai-beginner-showcase/static/js/2.50b8c935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8350c793be79cae552",
+    "url": "/western-ai-beginner-showcase/static/js/main.92a6eb3b.chunk.js"
+  },
+  {
+    "revision": "275e6b8ecfeeb3e3d168",
+    "url": "/western-ai-beginner-showcase/static/js/runtime-main.b3625252.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/western-ai-beginner-showcase/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "82b82ccafdf5cb8e1a5801c7502adc01",
+    "url": "/western-ai-beginner-showcase/static/media/cancer-detection.82b82cca.jpg"
+  }
+]);
